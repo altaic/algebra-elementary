d65433c10a0d22d6306faf4391e0a40931121fdb
William Knop <william.knop.nospam@gmail.com>
2015-09-27 21:11:14 -0400

    Test-suite tweaks for Unique cases (still misbehaving)

--- testsuite/Tasty.hs
+++ testsuite/Tasty.hs
@@ -16,12 +16,14 @@
 
 module Main where
 
-import Test.Tasty
-import Test.Tasty.HUnit
+import           Test.Tasty
+import           Test.Tasty.HUnit
 -- import Test.Tasty.SmallCheck as SC
-import Test.Tasty.QuickCheck as QC
-import Numeric.Algebra.Elementary.AST as A
-import Numeric.Algebra.Elementary.Rewrite as R
+import qualified Data.Unique                        as U
+import           Numeric.Algebra.Elementary.AST     as A
+import           Numeric.Algebra.Elementary.Rewrite as R
+import qualified System.IO.Unsafe                   as US
+import           Test.Tasty.QuickCheck              as QC
 
 
 main :: IO ()
@@ -35,13 +37,22 @@ properties = localOption (mkTimeout 500000) (testGroup "QuickCheck Properties" [
 
 qcMathSimplifier :: TestTree
 qcMathSimplifier = testGroup "Simplifier"
-  [ QC.testProperty "Simplify is Canonical" $ \e -> R.simplify e == R.simplify (R.simplify e) ]
+  [ QC.testProperty "Simplify Is Canonical" $ \e -> R.simplify e == R.simplify (R.simplify e) ]
 
 unitTests :: TestTree
 unitTests = testGroup "Unit Tests" [utMathSimplifier, utMathOther]
 
 algVars :: [A.Expr]
 algVars = map (\c -> A.mkVar [c]) ['a'..'z']
+
+algSameIds :: [A.Id]
+algSameIds = [A.mkId "a", A.mkId "a"]
+
+algSameVars :: [A.Expr]
+algSameVars = [A.mkVar "a", A.mkVar "a"]
+
+uniques :: [U.Unique]
+uniques = [US.unsafePerformIO U.newUnique, US.unsafePerformIO U.newUnique]
 
 utMathSimplifier :: TestTree
 utMathSimplifier = testGroup "Simplifier" [
@@ -94,11 +105,15 @@ utMathOther = testGroup "Other" [
                                                @?= False,
   testCase "Error Checking (simplified, pass)" $ A.check (R.simplify (A.Exp (A.Coeff 2) (algVars!!23)))
                                                @?= True,
-  testCase "Equality (pass)"                   $ A.Exp (A.Coeff 2) (algVars!!23) == A.Exp (A.Coeff 2) (algVars!!23)
-                                               @?= True,
+  testCase "Equality (pass)"                   $ A.Exp (A.Coeff 2) (algVars!!23)
+                                               @?= A.Exp (A.Coeff 2) (algVars!!23),
   testCase "Equality (fail-1)"                 $ A.Exp (A.Coeff 3) (algVars!!23) == A.Exp (A.Coeff 2) (algVars!!23)
                                                @?= False,
   testCase "Equality (fail-2)"                 $ A.Exp (A.Coeff 2) (A.Mult [algVars!!23, algVars!!24]) == A.Exp (A.Coeff 2) (algVars!!23)
                                                @?= False,
-  testCase "Uniques Are Unique"                $ A.mkId "x" /= A.mkId "x"
+  testCase "Uniques Are Unique"                $ uniques!!0 /= uniques!!1
+                                               @?= True,
+  testCase "Ids Are Unique"                    $ algSameIds!!0 /= algSameIds!!1
+                                               @?= True,
+  testCase "Vars Are Unique"                   $ algSameVars!!0 /= algSameVars!!1
                                                @?= True ]
