f3c24f0c0ad9c6a34be1a6b92480d4119e00511a
William Knop <william.knop.nospam@gmail.com>
2015-09-28 20:33:28 -0400

    Removed cruft

--- src/Numeric/Algebra/Elementary/Pretty.hs
+++ src/Numeric/Algebra/Elementary/Pretty.hs
@@ -26,21 +26,3 @@ import qualified Text.Nicify                    as N
 
 prettyPrint :: Expr -> String
 prettyPrint e = N.nicify (show e)
-
-
-
--- -- | Pretty-prints an 'Expr'.
--- instance Show Expr where
---   show (Coeff c       ) = show (fromRational c :: Double)
---   show (Var v         ) = show v
---   show (FunId i (v:vs)) = show i ++ "(" ++ L.foldr (\vv acc -> acc ++ ", " ++ show vv) (show v) vs ++ ")"
---   show (FunId i []    ) = show i ++ "()"
---   show (Mult []       ) = "()"
---   show (Mult [e]      ) = "(" ++ show e ++ ")"
---   show (Mult (e:es)   ) = "(" ++ show e ++ "⋅" ++ show (Mult es) ++ ")"
---   show (Add []        ) = "{}"
---   show (Add [e]       ) = "{" ++ show e ++ "}"
---   show (Add (e:es)    ) = show e ++ " + " ++ show (Add es)
---   show (Exp b e       ) = show b ++ "^(" ++ show e ++ ")"
---   show (Log b e       ) = "log_[" ++ show b ++ "](" ++ show e ++ ")"
---   show (Error s       ) = "[Error: " ++ s ++ "]"
