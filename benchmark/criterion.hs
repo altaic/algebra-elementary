0cab200edac442e6a545aa301d020e8fc4f78181
William Knop <william.knop.nospam@gmail.com>
2015-09-29 00:31:14 -0400

    Benchmarking stub (not working yet), ref #11

---
+++ benchmark/criterion.hs
@@ -0,0 +1,19 @@
+{-# OPTIONS_GHC -fdefer-typed-holes -fno-warn-orphans #-}
+
+
+
+module Main (main) where
+
+
+
+import Numeric.Algebra.Elementary.AST
+import Numeric.Algebra.Elementary.Rewrite
+import Criterion.Main
+
+
+main :: IO ()
+main = defaultMain [ bgroup "rewrite" [ bench "?"   $ whnf simplify _
+                                      , bench "??"  $ whnf simplify _
+                                      , bench "???" $ whnf simplify _
+                                      ]
+                   ]
